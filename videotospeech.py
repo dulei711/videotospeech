2a0fd863b5d4d84b88cff0f6f9ca7a1c6f17d8a8
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-14 17:47:36 -0300

    Update videotospeech.py

--- videotospeech.py
+++ videotospeech.py
@@ -1,41 +1,64 @@
 import streamlit as st
 import moviepy.editor as mp
-from wordcloud import WordCloud, STOPWORDS
+import speech_recognition as sr
 import matplotlib.pyplot as plt
 
-
-def generate_wordcloud(audio_text):
-    # Create a WordCloud object
-    stopwords = set(STOPWORDS)
-    wordcloud = WordCloud(width = 800, height = 800, background_color ='white', stopwords = stopwords, min_font_size = 10).generate(audio_text)
-    
-    # Plot the WordCloud image
-    plt.figure(figsize = (8, 8), facecolor = None)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad = 0)
-    
-    # Return the plot as an image
-    return plt
-
-
-# Set up the Streamlit app
-st.set_page_config(page_title="Video to WordCloud", page_icon=":guardsman:", layout="wide")
-st.title("Convert a video to a WordCloud")
-
-# Add a file uploader to the app
-uploaded_file = st.file_uploader("Choose an MP4 video file", type=["mp4", "mov"])
-
-# Check if a file has been uploaded
-if uploaded_file is not None:
-    # Use moviepy to extract audio from the video
+@st.cache_data
+def generate_audio(uploaded_file):
     video = mp.VideoFileClip(uploaded_file)
     audio = video.audio
     audio_file = "temp_audio.wav"
     audio.write_audiofile(audio_file)
+    st.success('Vídeo convertido em áudio!', icon="✅")
+    return audio_file
 
+@st.cache_data
+def transcribe_audio(audio_file, language):
+    # Upload an audio file
+    audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
+    # Select the language of origin
+    language = st.selectbox("Select the language:", ["English", "French", "German", "Spanish"])
+    # Convert the language name to a language code
+    if language == "English":
+        language_code = "en-US"
+    elif language == "French":
+        language_code = "fr-FR"
+    elif language == "German":
+        language_code = "de-DE"
+    elif language == "Spanish":
+        language_code = "es-ES"
+    # Create a new recognizer object
+    r = sr.Recognizer()
+    # Open the audio file and read its contents into memory
+    with sr.AudioFile(audio_file) as source:
+        audio = r.record(source)
+    # Transcribe the audio file using Google Speech Recognition
+    try:
+        text = r.recognize_google(audio, language=language)
+        st.success('Áudio convertido em texto!', icon="✅")
+    except sr.UnknownValueError:
+        text = "Unable to transcribe audio"
+    # Return the transcribed text
+    return text
+
+# Set up the Streamlit app
+st.set_page_config(page_title="BookTalker: 📺➡📖", page_icon=":guardsman:", layout="wide")
+st.title("Convert your talks into best-sellers!")
+
+# Add a file uploader to the app
+uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "mov"])
+
+# Check if a file has been uploaded
+if uploaded_file is not None:
+    # Use moviepy to extract audio from the video
+    generate_audio(uploaded_file)
     # Use Google Speech Recognition API to convert audio to text
-    audio_text = "TODO: use a speech-to-text API to get the text from the audio file"
-
-    # Generate the WordCloud and display it in Streamlit
-    st.pyplot(generate_wordcloud(audio_text))
+    transcribe_audio(audio_file, language)
+    if audio_file is not None:
+            text = transcribe_audio(audio_file, language=language_code)
+            txt = st.text_area('Transcription', text)
+    else:
+        st.error('Some error with the audio', icon="🚨")
+else:
+    st.error('Some error with the video', icon="🚨")  
+    
