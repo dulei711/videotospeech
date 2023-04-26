6a20f409a25511565d2ab1340d1ff951f74e910c
Thiago Ferraz <33188649+dulei711@users.noreply.github.com>
2023-04-26 15:07:04 -0300

    Update videotospeech.py

--- videotospeech.py
+++ videotospeech.py
@@ -1,61 +1,47 @@
-import streamlit as st
-import moviepy.editor as mp
-import speech_recognition as sr
+# importing libraries 
+import numpy as np
 import matplotlib.pyplot as plt
+import moviepy.editor as mp
+import speech_recognition as sr 
+import os 
+from os import path
+from pydub import AudioSegment
+from pydub.silence import split_on_silence
+from pathlib import Path
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-@st.cache_data
-def generate_audio(uploaded_file):
-    video = mp.VideoFileClip(uploaded_file)
-    audio = video.audio
-    audio_file = "temp_audio.wav"
-    audio.write_audiofile(audio_file)
-    st.success('Vídeo convertido em áudio!', icon="✅")
-     # Select the language of origin
-    language = st.selectbox("Select the language:", ["English", "French", "German", "Spanish"])
-    # Convert the language name to a language code
-    if language == "English":
-        language_code = "en-US"
-    elif language == "French":
-        language_code = "fr-FR"
-    elif language == "German":
-        language_code = "de-DE"
-    elif language == "Spanish":
-        language_code = "es-ES"
-    return audio_file, language
+def get_large_audio_transcription(path, lang):
+    sound = AudioSegment.from_wav(path)  
+    chunks = split_on_silence(sound, min_silence_len = 500, silence_thresh = sound.dBFS-14, keep_silence=500)
+    folder_name = "audio-chunks"
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+    whole_text = ""
+    for i, audio_chunk in enumerate(chunks, start=1):
+        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
+        audio_chunk.export(chunk_filename, format="wav")
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            try:
+                text = r.recognize_google(audio_data=audio_listened, language=lang)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                print(chunk_filename, ":", text)
+                whole_text += text
+    return whole_text
 
-@st.cache_data
-def transcribe_audio(audio_file, language):
-    # Create a new recognizer object
-    r = sr.Recognizer()
-    # Open the audio file and read its contents into memory
-    with sr.AudioFile(audio_file) as source:
-        audio = r.record(source)
-    # Transcribe the audio file using Google Speech Recognition
-    try:
-        text = r.recognize_google(audio, language=language)
-        st.success('Áudio convertido em texto!', icon="✅")
-    except sr.UnknownValueError:
-        text = "Unable to transcribe audio"
-    # Return the transcribed text
-    return text
-
-# Set up the Streamlit app
-st.set_page_config(page_title="BookTalker: 📺➡📖", page_icon=":guardsman:", layout="wide")
-st.title("Convert your talks into best-sellers!")
-
-# Add a file uploader to the app
-uploaded_file = st.file_uploader("Choose a video file", type=["mp4", "mov"])
-if st.button("Run"):
-    # Check if a file has been uploaded
-    if uploaded_file is not None:
-        # Use moviepy to extract audio from the video
-        generate_audio(uploaded_file)
-        # Use Google Speech Recognition API to convert audio to text
-        transcribe_audio(audio_file, language)
-        if audio_file is not None:
-                text = transcribe_audio(audio_file, language=language_code)
-                txt = st.text_area('Transcription', text)
-        else:
-            st.error('Some error with the audio', icon="🚨")
-    else:
-        st.error('Some error with the video', icon="🚨") 
+st.title(VideoToSpeech)
+video_file = st.file_uploader("Choose a file", type=['mov', 'mp4'])
+if video_file is not None:
+    lang = st.radio("What's the language?", ('en-US', 'pt-BR'))
+    if lang:
+        if st.button("Run"):
+            clip = mp.VideoFileClip(video_file).subclip(0)
+            clip.audio.write_audiofile("theaudio_part.wav")
+            r = sr.Recognizer()
+            path = "/content/theaudio_part.wav"
+            text = get_large_audio_transcription(path, lang)
+            st.write(text)
